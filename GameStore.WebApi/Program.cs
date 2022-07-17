abc8a754d833cdc0892806e13994e4855aa1e941
SnEG1R <steamsnegir@gmail.com>
2022-07-17 15:53:54 +0300

    add dependency injection

--- GameStore.WebApi/Program.cs
+++ GameStore.WebApi/Program.cs
@@ -1,10 +1,38 @@
+using System.Reflection;
+using GameStore.Application;
+using GameStore.Application.Common.Mappings;
+using GameStore.Application.Interfaces;
+using GameStore.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IGameStoreDbContext).Assembly));
+});
+builder.Services.AddPersistence(builder.Configuration);
+builder.Services.AddApplication();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<GameStoreDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+}
 
 if (app.Environment.IsDevelopment())
 {
