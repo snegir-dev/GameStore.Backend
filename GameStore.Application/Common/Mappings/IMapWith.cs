7ec580e7436eac6278e31c2291042f2d90bd5837
SnEG1R <steamsnegir@gmail.com>
2022-07-17 15:20:15 +0300

    add mapping

---
+++ GameStore.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace GameStore.Application.Common.Mappings;
+
+public interface IMapWith<T>
+{
+    void Mapping(Profile profile) =>
+        profile.CreateMap(typeof(T), GetType());
+}
