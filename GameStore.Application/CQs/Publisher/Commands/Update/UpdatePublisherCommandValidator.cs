152e2b5975ae00ae0ad78940947316b634815bdf
SnEG1R <steamsnegir@gmail.com>
2022-07-25 19:49:30 +0300

    add validators for commands and queries for publisher

---
+++ GameStore.Application/CQs/Publisher/Commands/Update/UpdatePublisherCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace GameStore.Application.CQs.Publisher.Commands.Update;
+
+public class UpdatePublisherCommandValidator : AbstractValidator<UpdatePublisherCommand>
+{
+    public UpdatePublisherCommandValidator()
+    {
+        RuleFor(p => p.Id).NotEmpty();
+        RuleFor(p => p.Name).NotEmpty().MaximumLength(255);
+        RuleFor(p => p.Description).NotEmpty().MaximumLength(600);
+        RuleFor(p => p.DateFoundation).NotEmpty();
+    }
+}
