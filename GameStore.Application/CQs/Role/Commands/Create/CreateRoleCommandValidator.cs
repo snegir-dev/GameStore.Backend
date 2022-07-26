48731e99a80f0bcfccab6369f95ba69f2b51ccd4
SnEG1R <steamsnegir@gmail.com>
2022-07-26 23:25:06 +0300

    add validators for commands and queries for role

---
+++ GameStore.Application/CQs/Role/Commands/Create/CreateRoleCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace GameStore.Application.CQs.Role.Commands.Create;
+
+public class CreateRoleCommandValidator : AbstractValidator<CreateRoleCommand>
+{
+    public CreateRoleCommandValidator()
+    {
+        RuleFor(r => r.Name).NotEmpty().MaximumLength(255);
+    }
+}
---
+++ GameStore.Application/CQs/Role/Commands/Delete/DeleteRoleCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace GameStore.Application.CQs.Role.Commands.Delete;
+
+public class DeleteRoleCommandValidator : AbstractValidator<DeleteRoleCommand>
+{
+    public DeleteRoleCommandValidator()
+    {
+        RuleFor(r => r.Id).NotEmpty();
+    }
+}
